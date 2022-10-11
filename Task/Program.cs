c3512f5bc874b156c6d6727112e9aea55ad614a4
Inav Zakharchenko <zakharka2000@ya.ru>
2022-10-11 20:22:34 +0300

    реаизован основной метод SeparatedArray

--- Task/Program.cs
+++ Task/Program.cs
@@ -1,7 +1,34 @@
-﻿// Запрос данных пользователяю, принимает строку заголовок.
+﻿//==========================================
+//Написать программу, которая из имеющегося массива строк формирует
+//массив из строк, длина которых меньше либо равна 3 символа.
+//==========================================
+
+// Запрос данных пользователяю, принимает строку заголовок.
 // Возврашает массив введенных слов.
-string [] ReadData(string greeting = "Введите данные")
+string[] ReadData(string greeting = "Введите данные")
 {
     Console.WriteLine(greeting);
     return (Console.ReadLine() ?? "0").Split(",");
+}
+
+// Возвращает массив с элементами длина которых меньше либо равна 3 символа.
+string[] SeparatedArray(string[] array)
+{
+    string[] newArray = Array.Empty<string>();
+    foreach (string item in array)
+    {
+        if (item.Length <= 3)
+        {
+            newArray = AddArray(array:newArray, data:item);
+        }
+    }
+    return newArray;
+}
+
+//Добавляет элемент в массив
+string[] AddArray(string[] array, string data)
+{
+    Array.Resize(ref array, array.Length+1);
+    array[array.Length]=data;
+    return array;
 }
